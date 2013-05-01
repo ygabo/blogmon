7dc71d79483f0c7005173cce38f368c7aaf90cc7
ygabo <yelnil@gmail.com>
2013-05-01 15:34:56 -0700

    Route fix for root.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Blog::Application.routes.draw do
   authenticated :user do
     root :to => 'posts#index'
   end
-  root :to => "posts#index", :param_name => "dash"
+  root :to => "posts#index"
   
   devise_for :users, :controllers => { :registrations => "registrations" }
   resources :users
