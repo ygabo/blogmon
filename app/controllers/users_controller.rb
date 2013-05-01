1e46d51dc1545e609dae934c32c17891a3d1dd61
ygabo <yelnil@gmail.com>
2013-05-01 14:57:27 -0700

    Some random fixes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   def index
     authorize! :index, @user, :message => 'Not authorized as an administrator.'
     @users = User.all
-    @my_friends = current_user.friends
     @my_friendships = current_user.friendships
+    @my_friends = current_user.friends
   end
 
   def show
